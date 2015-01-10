067427ca216199dfda42c0641e93d5e382cdd226
corentin-R <corentinraoult@laposte.net>
2015-01-10 14:20:20 -0500

    hardcode mobile devices

--- js/main.js
+++ js/main.js
@@ -24,11 +24,6 @@ var shootPath = 'res/shoot.mp3';
 
 var JOUEUR = 1;
 var ENNEMY  = 2;
-window.mobilecheck = function() {
-	var check = false
-	(function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))window.location=b})(navigator.userAgent||navigator.vendor||window.opera,'http://detectmobilebrowser.com/mobile');
-	return check;
-}
 
 
 window.onload = function() {
@@ -55,7 +50,7 @@ window.onload = function() {
         }
         var game = new Game(largeurBase, hauteurBase);
 
-        if(window.mobileCheck){
+
         	game.preload(
         		playerSheetPath,
         		enemy1SheetPath,
@@ -63,25 +58,8 @@ window.onload = function() {
         		enemy3SheetPath,
         		enemy4SheetPath,
         		projectileSheetPath,
-        		starSheetPath);
-        }
-        else{
-        	game.preload(
-        		playerSheetPath,
-        		enemy1SheetPath,
-        		enemy2SheetPath,
-        		enemy3SheetPath,
-        		enemy4SheetPath,
-        		projectileSheetPath,
-        		starSheetPath,
-        		'res/Hit.mp3',
-        		bgmPath,
-        		gameOverPath,
-        		hitPath,
-        		shootPath,
-        		explosionSheetPath,
-        		FCIPath);
-        }
+        		starSheetPath,explosionSheetPath);
+
 
         game.fps = 60;
         game.scale=scale;
@@ -173,11 +151,7 @@ window.onload = function() {
                 this.deltaAppartion = 2.0;
                 this.deltaAppartionStar = 0.8;
 
-                if(!window.mobileCheck){
-	                // Start BGM
-	                this.bgm = game.assets[bgmPath];
-	                this.bgm.play();
-	            }
+
 	        },
 
 	        handleTouchControl: function (evt) {
@@ -282,12 +256,6 @@ window.onload = function() {
                     		}
                     	}
                     }
-             if(!window.mobileCheck){
-	           // Loop BGM
-	           if( this.bgm.currentTime >= this.bgm.duration ){
-	           	this.bgm.play();
-	           }
-	       }
        },
 
        setScore: function (value) {
@@ -381,9 +349,6 @@ window.onload = function() {
         	this.shootDuration = 0 ;
         	this.parentNode.shoot++;
         	this.parentNode.setShoot(this.parentNode.shoot);
-        	 if(!window.mobileCheck){
-        		Game.instance.assets[shootPath].play();
-        	}
         },
 
         destroyed: function(proj) {
@@ -391,10 +356,6 @@ window.onload = function() {
         	explo = new Explostion(this.x, this.y, JOUEUR, this.width);
         	this.parentNode.addChild(explo);
         	proj.parentNode.removeChild(proj);
-        	 if(!window.mobileCheck){
-        		game.assets[hitPath].play();
-        	}
-
         }
 
     });
@@ -504,9 +465,6 @@ destroyed1: function(proj) {
 	var game = Game.instance;
 	this.parentNode.parentNode.setScore(this.parentNode.parentNode.score + 1);
 	this.parentNode.parentNode.penguin.projectileGroup.removeChild(proj);
-	 if(!window.mobileCheck){
-		game.assets[hitPath].play();
-	}
 	explo = new Explostion(this.x, this.y, ENNEMY, this.width);
 	this.parentNode.parentNode.addChild(explo);
 	this.actuallyDestoyed = true;
@@ -611,9 +569,6 @@ var Explostion = enchant.Class.create(enchant.Sprite, {
     		{
     			if(this.faction==JOUEUR){
     				var game = Game.instance;
-    				if(!window.mobileCheck){
-    					this.parentNode.bgm.stop();
-    				}
     				game.replaceScene(new SceneGameOver(this.parentNode.score,this.parentNode.shoot));   
     			}
     			this.parentNode.removeChild(this);
@@ -658,13 +613,6 @@ var Star = enchant.Class.create(enchant.Sprite, {
 
  		var game;
  		game = Game.instance;
-
- 		 if(!window.mobileCheck){
-	        // Background music
-	        this.gom = game.assets[gameOverPath]; // Add this line
-	        // Start BGM
-	        this.gom.play();
-	    }
 
         gameOverLabel = new Label("GAME OVER<br>Tap to Restart");
         gameOverLabel.x = game.width/2-gameOverLabel.width/2;
@@ -711,9 +659,7 @@ var Star = enchant.Class.create(enchant.Sprite, {
 
     touchToRestart: function(evt) {
     	var game = Game.instance; 
-    	if(!window.mobileCheck){
-    		this.gom.stop();
-    	}
+
     	scene = new SceneGame();
     	game.replaceScene(scene);
     }
